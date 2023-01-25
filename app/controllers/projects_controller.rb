c26a9b6938a74370b2c543e154d7c44bf0cc34c5
benjamin treglown <treggy89@gmail.com>
2023-01-25 13:41:16 +1100

    Create new and display projects on side bar with an area to display tickets completed

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,44 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+end

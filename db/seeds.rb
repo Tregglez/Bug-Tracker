84ce2d795b44ea5d7a68f4b38e6bf7b7fa06a182
benjamin treglown <treggy89@gmail.com>
2023-01-26 17:46:18 +1100

    Added colors to the front page with main info box now resembling a sticky note. Area for title is now responsive for any length of title.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ Project.destroy_all
 puts "Creating Projects"
 Project.create(name: "Project 1")
 Project.create(name: "Bug Tracker")
+
+puts "creating users"
+User.create(email: "ben@bugtracker.com", password: "secret")
